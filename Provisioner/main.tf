bf229e9aaed6abf857cbd4c785f91328a823be4f
Sanjay Maredla <saisanjay1992@gmail.com>
2024-07-30 16:42:40 +0200

    Update main.tf

--- Provisioner/main.tf
+++ Provisioner/main.tf
@@ -10,6 +10,7 @@ resource "aws_subnet" "sub1" {
   vpc_id = aws_vpc.terraform_vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "ap-south-2a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "My_Ig" {
@@ -42,7 +43,7 @@ resource "aws_security_group" "webSG"  {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["10.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
@@ -50,7 +51,7 @@ resource "aws_security_group" "webSG"  {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["10.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
 
@@ -58,7 +59,7 @@ resource "aws_security_group" "webSG"  {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["10.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     tags = {
@@ -69,20 +70,20 @@ resource "aws_security_group" "webSG"  {
 
 resource "aws_key_pair" "demo_key_pair" {
   key_name = "demo_key_pair"
-  public_key = file("~/.ssh/public_key.pub")
+  public_key = file("public_key.pub")
 }
 
 resource "aws_instance" "server" {
   ami = "ami-05b5693ff73bc6f84"
   instance_type = "t3.micro"
-  key_name = aws_key_pair.demo_key_pair
+  key_name = aws_key_pair.demo_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.webSG.id]
   subnet_id = aws_subnet.sub1.id
 
     connection {
       type = "ssh"
       user = "ubuntu"
-      private_key = file("~/.ssh/public_key")
+      private_key = file("public_key")
       host = self.public_ip
     }
 
