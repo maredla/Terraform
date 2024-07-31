2b6879561f62a7404d64194de8add28891bf4310
Sanjay Maredla <saisanjay1992@gmail.com>
2024-07-31 11:36:13 +0200

    Create main.tf

---
+++ Workspaces/modules/EC2_instance/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+variable "ami" {
+  description = "this is the AMI value for the instance"
+}
+
+variable "instance_type" {
+  description = "this is the instance type for the EC2 instance"
+}
+
+resource "aws_instance" "example" {
+  ami = var.ami
+  instance_type = var.instance_type
+}
